5c8229eb305809fe6f506c39ab953ceaf45879c2
Niall Beard <niallbeard@gmail.com>
2015-10-30 16:51:02 +0000

    Mount engine at root

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :books
-  mount Tate::Engine => "/tate"
+  mount Tate::Engine => "/"
+
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013161807) do
+ActiveRecord::Schema.define(version: 20151029115336) do
 
   create_table "books", force: :cascade do |t|
     t.text     "name"
@@ -22,20 +22,20 @@ ActiveRecord::Schema.define(version: 20151013161807) do
 
   create_table "tate_annotation_attributes", force: :cascade do |t|
     t.string   "name",       null: false
+    t.string   "identifier", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "tate_annotation_attributes", ["name"], name: "index_tate_annotation_attributes_on_name"
+  add_index "tate_annotation_attributes", ["name", "identifier"], name: "index_tate_annotation_attributes_on_name_and_identifier"
 
   create_table "tate_annotation_value_seeds", force: :cascade do |t|
-    t.integer  "attribute_id",                                       null: false
+    t.integer  "attribute_id",                                            null: false
     t.string   "old_value"
-    t.string   "annotation_attributes",                              null: false
-    t.string   "identifier",                                         null: false
-    t.string   "string",                                             null: false
-    t.string   "value_type",            limit: 50, default: "FIXME", null: false
-    t.integer  "value_id",                         default: 0,       null: false
+    t.string   "tate_annotation_attributes",                              null: false
+    t.string   "identifier",                                              null: false
+    t.string   "value_type",                 limit: 50, default: "FIXME", null: false
+    t.integer  "value_id",                              default: 0,       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -43,17 +43,17 @@ ActiveRecord::Schema.define(version: 20151013161807) do
   add_index "tate_annotation_value_seeds", ["attribute_id"], name: "index_tate_annotation_value_seeds_on_attribute_id"
 
   create_table "tate_annotation_versions", force: :cascade do |t|
-    t.integer  "annotation_id",                                       null: false
-    t.integer  "version",                                             null: false
+    t.integer  "annotation_id",                                             null: false
+    t.integer  "version",                                                   null: false
     t.integer  "version_creator_id"
-    t.string   "source_type",                                         null: false
-    t.integer  "source_id",                                           null: false
-    t.string   "annotatable_type",   limit: 50,                       null: false
-    t.integer  "annotatable_id",                                      null: false
-    t.integer  "attribute_id",                                        null: false
+    t.string   "source_type",                                               null: false
+    t.integer  "source_id",                                                 null: false
+    t.string   "annotatable_type",   limit: 50,                             null: false
+    t.integer  "annotatable_id",                                            null: false
+    t.integer  "attribute_id",                                              null: false
     t.string   "old_value"
     t.string   "value_type",         limit: 50, default: "Tate::TextValue", null: false
-    t.integer  "value_id",                      default: 0,           null: false
+    t.integer  "value_id",                      default: 0,                 null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -61,15 +61,15 @@ ActiveRecord::Schema.define(version: 20151013161807) do
   add_index "tate_annotation_versions", ["annotation_id"], name: "index_tate_annotation_versions_on_annotation_id"
 
   create_table "tate_annotations", force: :cascade do |t|
-    t.string   "source_type",                                         null: false
-    t.integer  "source_id",                                           null: false
-    t.string   "annotatable_type",   limit: 50,                       null: false
-    t.integer  "annotatable_id",                                      null: false
-    t.integer  "attribute_id",                                        null: false
+    t.string   "source_type",                                               null: false
+    t.integer  "source_id",                                                 null: false
+    t.string   "annotatable_type",   limit: 50,                             null: false
+    t.integer  "annotatable_id",                                            null: false
+    t.integer  "attribute_id",                                              null: false
     t.string   "old_value"
     t.string   "value_type",         limit: 50, default: "Tate::TextValue", null: false
-    t.integer  "value_id",                      default: 0,           null: false
-    t.integer  "version",                                             null: false
+    t.integer  "value_id",                      default: 0,                 null: false
+    t.integer  "version",                                                   null: false
     t.integer  "version_creator_id"
     t.datetime "created_at"
     t.datetime "updated_at"
